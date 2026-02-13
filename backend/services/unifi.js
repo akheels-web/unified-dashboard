86b5669332fd2cf81a59e25bc5b702332d53ac0a
Akheel <akheel@example.com>
2026-02-13 12:38:18 +0530

    ppp

--- backend/services/unifi.js
+++ backend/services/unifi.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
 const axios = require('axios');
 
 const API_KEY = process.env.UNIFI_API_KEY;
