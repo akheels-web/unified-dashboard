ca947c353482e25ce1df5a32501c95e7b69b0626
Akheel <akheel@example.com>
2026-02-14 15:06:07 +0530

    ppp

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
+
 const pool = require('./db');
 const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '.env') });
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
