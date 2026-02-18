03f54f8abbf3d5e6a9874bfd1cf91e804fa74bec
akheels-web <akheelbm@gmail.com>
2026-02-18 15:59:54 +0530

    Update authConfig.ts

--- app/src/authConfig.ts
+++ app/src/authConfig.ts
@@ -5,8 +5,8 @@ export const msalConfig: Configuration = {
     auth: {
         clientId: import.meta.env.VITE_AZURE_CLIENT_ID || "Enter_the_Application_Id_Here",
         authority: `https://login.microsoftonline.com/${import.meta.env.VITE_AZURE_TENANT_ID || "common"}`,
-        redirectUri: "/",
-        postLogoutRedirectUri: "/"
+        redirectUri: window.location.origin,
+        postLogoutRedirectUri: window.location.origin
     },
     cache: {
         cacheLocation: "sessionStorage", // This configures where your cache will be stored
